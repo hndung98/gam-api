e0ba3bd15da5613fffd6f6e6f189a6ade9e9210d
hndung98 <54524235+hndung98@users.noreply.github.com>
2025-08-28 22:20:33 +0700

    fix(user-entity): remove invalid key

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { User } from '@prisma/client';
 
-export const UserOmittedKeys = ['password', 'createdAt', 'updatedAt'] as const;
+export const UserOmittedKeys = ['createdAt', 'updatedAt'] as const;
 type UserKeysUnion = (typeof UserOmittedKeys)[number];
 
 export type UserEntity = Omit<User, UserKeysUnion>;
