4ab807dbe3e37abdaf6ef23a35f7a7bb4a904e00
hndung98 <54524235+hndung98@users.noreply.github.com>
2025-08-30 00:29:41 +0700

    chore(cors): edit whitelist for local test

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,7 +1,10 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 const whitelist = [
-  'http://localhost:3000', // frontend dev
+  'http://localhost:3000', // localhost 3000
+  'http://127.0.0.1:3000', // local IP 3000
+  'http://localhost:5000', // localhost 5000
+  'http://127.0.0.1:5000', // local IP 5000
   'https://staging.mydomain.com', // staging
   'https://mydomain.com', // production
 ];
